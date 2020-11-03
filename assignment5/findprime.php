f267b261ffd830fa2ea501d924f903343cb9818f
Kate Rebello <katerebello27@gmail.com>
2020-11-03 20:34:06 +0530

    corrected

--- assignment5/findprime.php
+++ assignment5/findprime.php
@@ -3,20 +3,38 @@
 <head>
 	<title>PHP</title>
 </head>
+<style>
+	body{
+		border:1px solid black;
+		text-align:center;
+	}
+</style>
 <body>
 	<?php
 	 $num1 = $_GET["num1"];
 	 $num2 = $_GET["num2"];
-
+	 echo"<div>";
+	 function prime($num){
+		if($num == 1){
+			return 0;
+		}
+		for($i=2;$i<$num;$i++){
+			
+			if(($num%$i)==0){
+				return 0;
+			}
+		}
+		return 1;
+	 }
 	 echo "<h1>Prime numbers between ". $num1 ." and " . $num2 . " are:</h1>";
 	 for($i=$num1;$i<=$num2;$i++){
-	 	if(($i%2) != 0){
-	 		if($i != 1){
-	 			echo "<h2>" .$i . "</h2>";
-	 		}
+		$ans = prime($i);
+	 	if($ans == 1){
+	 		echo "<h3>" .$i . "</h3>";
 	 	}
 
 	 }
+	 echo"</div>";
  ?>
 <br>
 
